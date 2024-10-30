f57d581241a55ce691486151c73630553a144e99
Romain-Plmg <plumaugat.romain78@gmail.com>
2024-10-30 11:20:41 +0100

    Add project files.

---
+++ KeplerSim/KeplerSim.cpp
@@ -0,0 +1,12 @@
+﻿// KeplerSim.cpp : Defines the entry point for the application.
+//
+
+#include "KeplerSim.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
